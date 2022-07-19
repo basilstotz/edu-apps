00fa4c4d01249b264068959abef7412fa6b606e1
Basil Stotz <basil.stotz@gmail.com>
2022-07-19 20:10:06 +0200

    some fixes

---
+++ patch/basis/bin.d/set-sources-list.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+cat <<EOF >/etc/apt/sources.list
+# See https://wiki.debian.org/SourcesList for more information.
+deb http://deb.debian.org/debian bullseye main contrib non-free
+deb-src http://deb.debian.org/debian bullseye main contrib non-free
+
+deb http://deb.debian.org/debian bullseye-updates main contrib non-free
+deb-src http://deb.debian.org/debian bullseye-updates main contrib non-free
+
+deb http://security.debian.org/debian-security/ bullseye-security main contrib non-free
+deb-src http://security.debian.org/debian-security/ bullseye-security main contrib non-free
+
+deb http://deb.debian.org/debian bullseye-backports main contrib non-free
+deb-src http://deb.debian.org/debian bullseye-backports main contrib non-free
+EOF
