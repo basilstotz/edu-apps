161e1f0aab3c51307211007d4d776a28c5f63301
Basil Stotz <basil.stotz@gmail.com>
2022-07-27 08:20:55 +0200

    add service-tunnel

---
+++ patch/basis/parts.d/service-tunnel/install.sh
@@ -0,0 +1,53 @@
+#!/bin/sh
+
+
+apt-get install autossh
+
+mkdir -p /etc/amxa/tunnel
+chmod 700 /etc/amxa
+mkdir -p /usr/local/bin
+
+cat <<EOF > /usr/local/bin/service-tunnel
+#!/bin/sh
+
+CONFIG="/etc/amxa/tunnel/tunnel.config"
+
+if test -e ${CONFIG}; then
+
+    . ${CONFIG}
+
+    chmod 600 ${KEY}
+
+    while true; do
+	if ping -c 1  ${HOST} 2>/dev/null >/dev/null; then
+            /usr/bin/autossh -M 0 -N -p ${PORT} -i ${KEY} ${OPTS} -R ${REMOTE}:localhost:22 ${USER}@${HOST}
+        fi
+	sleep 300
+    done
+else
+    sleep infinity
+fi
+EOF
+chmod +x /usr/local/bin/service-tunnel
+
+
+cat <<EOF > /etc/systemd/system/service-tunnel.service
+[Unit]
+Description=Tunnel for amXa
+After=syslog.target network.target nss-lookup.target
+
+[Service]
+Type=simple
+ExecStart=/usr/local/bin/service-tunnel
+#ExecReload=/bin/kill -s HUP $MAINPID
+#ExecStop=/bin/kill -s QUIT $MAINPID
+Restart=always
+RestartSec=300
+
+[Install]
+WantedBy=multi-user.target
+EOF
+systemctl enable service-tunnel.service
+
+
+exit
