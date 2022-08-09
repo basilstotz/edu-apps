d053db53cad36d8d7ccec06ce76ef243815346da
Basil Stotz <basil.stotz@gmail.com>
2022-08-09 13:25:11 +0200

    *

--- patch/basis/parts.d/wireguard/install.sh
+++ patch/basis/parts.d/wireguard/install.sh
@@ -3,7 +3,10 @@
 cd $(dirname $0)
 
 mkdir -p /etc/wireguard
-cp wg0.conf /etc/wireguard/wg0.conf
 
-systemctl enable wg-quick@wg0.service
+if test -f wg0.conf; then
+   cp wg0.conf /etc/wireguard/wg0.conf
+   systemctl enable wg-quick@wg0.service
+fi
+
 
