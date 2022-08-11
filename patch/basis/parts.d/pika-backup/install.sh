b8732c3bdba5823e4c6824a9c9fd1361b2f8cd1a
Basil Stotz <basil.stotz@gmail.com>
2022-08-11 11:29:13 +0200

    *

---
+++ patch/basis/parts.d/pika-backup/install.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+flatpak install flathub org.gnome.World.PikaBackup
+
+
+
+cat <<EOF > /usr/local/bin/pikabackup
+#!/bin/sh
+flatpak run org.gnome.World.PikaBackup
+EOF
+chmod +x /usr/local/bin/pikabackup
